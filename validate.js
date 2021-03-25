4c9a61abb1ec383a13ba0da952688457cc3a4fad
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-03-25 16:58:25 -0700

    finished validation

---
+++ validate.js
@@ -0,0 +1,10 @@
+function validate(){
+    let x = prompt("Do you like cigars?").toLowerCase()
+    if (x == "yes") {
+        alert ("Yeah you do!!");
+    } else if (x =="no" ) {
+        alert ("Boo!")
+    } else if (x =="yeah" ){
+        alert ("Of Course you do!!")
+    }
+} 
